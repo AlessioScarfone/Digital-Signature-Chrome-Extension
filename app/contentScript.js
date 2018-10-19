e548b56da95d0204b6048798f027002a04133a05
Alessio Scarfone <alessioscarfone93@gmail.com>
2018-10-19 22:52:39 +0200

    add zoom support

--- app/contentScript.js
+++ app/contentScript.js
@@ -1,37 +1,77 @@
 console.log("injected");
 //NOTA: 1 px = 0.75 point; 1 point = 1.333333 px
 
+const embedSize = {
+    startWidth: 0,
+    startHeight: 0,
+    roundingWidth: 30,
+    roundingHeight: 120
+}
+
+const bodySize = {
+    startWidth: 0,
+    startHeight: 0,
+    roundingWidth: 20,
+    roundingHeight: 0
+}
+
+var singatureNameNode = []; //store initial field setting [name , startX , startY]
+
 chrome.runtime.onConnect.addListener(function (port) {
     if (port.name == "content-script") {
         port.onMessage.addListener(function (msg) {
             console.log(msg);
-            let zoomval = (msg.newZoom - msg.oldZoom) * 100;
-            let _fields = Array.from(document.getElementsByClassName("field-name"));
-            console.log(_fields);
+            const zoomval = ((msg.newZoom - msg.oldZoom) * 100).toFixed(2);
+            const _fields = Array.from(document.getElementsByClassName("field-name"));
+            // console.log(_fields);
 
             console.log("zoomval:" + zoomval);
+
+            //adapt body size to zoom
+            const currentBodyWidth = parseFloat(document.body.style.width);
+            const currentBodyHeight = parseFloat(document.body.style.height);
+            document.body.style.width = addPercentage(currentBodyWidth, zoomval, bodySize.startWidth) + "pt";
+            document.body.style.height = addPercentage(currentBodyHeight, zoomval, bodySize.startHeight) + "pt";
+
+            //adapt embed size to zoom (px not needed)
+            const embed = document.getElementsByTagName("embed")[0];
+            const currentEmbedWidth = parseFloat(embed.width);
+            embed.width = addPercentage(currentEmbedWidth, zoomval, embedSize.startWidth, embedSize.roundingWidth);
+            const currentEmbedHeight = parseFloat(embed.height);
+            embed.height = addPercentage(currentEmbedHeight, zoomval, embedSize.startHeight, embedSize.roundingHeight);
+
+            //adapt fields name position to zoom
+            // console.log(singatureNameNode);
             _fields.forEach(el => {
-                console.log(el.textContent + " t:" + el.style.top + " l;" + el.style.left);
-                console.log((el.style.top + ((el.style.top / 100) * zoomval)) + "pt");
-                //togli i pt da el.style.X
-                let top = (el.style.top + ((el.style.top / 100) * zoomval));
-                let left = (el.style.left + ((el.style.left / 100) * zoomval));
-                el.style.top = top;
-                el.style.left = left;
-                console.log("NEW t:" + el.style.top + " l;" + el.style.left);
-            })
-
-
+                const f = singatureNameNode.find(field => field.name == el.textContent);
+                const top = parseFloat(el.style.top);
+                const left = parseFloat(el.style.left);
+                el.style.top = addPercentage(top, f.startY, zoomval) + "pt";
+                el.style.left = addPercentage(left, f.startX, zoomval) + "pt";
+            });
         });
     }
 });
+
+/**
+ * 
+ * @param {number} value - value on which sum the calculated percentage
+ * @param {number} percentage - percentage to calculate
+ * @param {number} start [start=value] - value on which to calculate the percentage
+ * @param {number} roundvalue [roundvalue=0] - value out of percentage
+ */
+function addPercentage(value, percentage, start = value, roundvalue = 0) {
+    value = value - roundvalue;
+    value = value + ((start / 100) * percentage);
+    return value + roundvalue;
+}
 
 chrome.storage.local.get(['fieldsData'], function (result) {
 
     var fieldData = result.fieldsData;
     console.log(fieldData);
     var fields = fieldData.fields;
-    console.log(fields);
+    // console.log(fields);
 
     const body = document.body;
     const embed = document.getElementsByTagName("embed")[0];
@@ -43,13 +83,18 @@ chrome.storage.local.get(['fieldsData'], function (result) {
     const pageCount = fieldData.page;
 
     //body size in pt
-    body.style.width = pw + 20 + "pt";
-    body.style.height = ph * pageCount + "pt";
+    body.style.width = pw + bodySize.roundingWidth + "pt";
+    body.style.height = (ph * pageCount) + bodySize.roundingHeight + "pt";
+    bodySize.startWidth = parseFloat(body.style.width) - bodySize.roundingWidth;
+    bodySize.startHeight = parseFloat(body.style.height) - bodySize.roundingHeight;
     body.style.overflow = ""
 
     //embed size in px
-    embed.width = topixel(pw) + 30;
-    embed.height = topixel(ph * pageCount) + 120;
+    embed.width = topixel(pw) + embedSize.roundingWidth;
+    embed.height = topixel(ph * pageCount) + embedSize.roundingHeight;
+
+    embedSize.startWidth = embed.width - embedSize.roundingWidth;
+    embedSize.startHeight = embed.height - embedSize.roundingHeight;
 
     fields.forEach(el => {
         let node = document.createElement("p");
@@ -66,6 +111,12 @@ chrome.storage.local.get(['fieldsData'], function (result) {
         node.style.left = x + "pt";
         node.classList.add("field-name");
         body.prepend(node);
+
+        singatureNameNode.push({
+            name: el.name,
+            startX: x,
+            startY: y
+        });
     });
 
 });
