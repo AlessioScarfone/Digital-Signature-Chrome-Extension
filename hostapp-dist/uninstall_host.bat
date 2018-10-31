c418309179577e14d5b0b0857ede218c7236aeed
Alessio Scarfone <alessioscarfone93@gmail.com>
2018-10-31 12:51:22 +0100

    fix bug in uninstall_host.bat

--- hostapp-dist/uninstall_host.bat
+++ hostapp-dist/uninstall_host.bat
@@ -3,5 +3,5 @@
 :: found in the LICENSE file.
 
 :: Deletes the entry created by install_host.bat
-REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.unical.digitalsignature.main" /f
-REG DELETE "HKLM\Software\Google\Chrome\NativeMessagingHosts\com.unical.digitalsignature.main" /f
+REG DELETE "HKCU\Software\Google\Chrome\NativeMessagingHosts\com.unical.digitalsignature.signer" /f
+REG DELETE "HKLM\Software\Google\Chrome\NativeMessagingHosts\com.unical.digitalsignature.signer" /f
